4458ac1913fa4c3155f6956b298811833fcaf25a
Ferry Febriansyah <perriebenhard@gmail.com>
2023-07-09 17:45:49 +0700

    feat: install flowbite

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+  <title>QiRin</title>
+</head>
+<body>
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl item-center shadow-lg flex space-x-4">
+        <div>
+          <div class="text-xl font-medium text-black">ChitChat</div>
+          <p class="text-slate-500">You have a new message!</p>
+        </div>
+      </div>
+</body>
+</html>
