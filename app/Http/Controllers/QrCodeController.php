d39c70433e1aaf233c00409754bdf47f6a444ea8
Ferry Febriansyah <perriebenhard@gmail.com>
2023-07-10 22:50:39 +0700

    feat: create QrCodeController

---
+++ app/Http/Controllers/QrCodeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+
+class QrCodeController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+  
+    public function create()
+    {
+        return view('generator');
+    }
+
+    public function generate(Request $request)
+    {
+        $link = $request->input('link');
+        $qrCode = QrCode::format('png')->size(200)->generate($link);
+        $imageData = 'data:image/png;base64,' . base64_encode($qrCode);
+
+        return view('generator')->with('imageData', $imageData);
+    }
+}
