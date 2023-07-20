d9ef89e8abeff494fb4d3ef7617e204bb3aa4694
Ferry Febriansyah <perriebenhard@gmail.com>
2023-07-20 21:06:56 +0700

    setup laravel breeze

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,31 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-    "./node_modules/flowbite/**/*.js"
-  ],
-  theme: {
-    container: {
-      center: true,
-      padding: '16px'
-    },
-    extend: {
-      colors: {
-        primary: '#149393'
-      },
-      fontFamily: {
-        inter: ['Inter']
-      },
-      screens: {
-        '2xl': '1320px'
-      }
-    },
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
 
+    theme: {
+        container: {
+            center: true,
+            padding: '16px'
+          },
+        extend: {
+            colors: {
+                primary: '#149393'
+              },
+              fontFamily: {
+                inter: ['Inter']
+              },
+              screens: {
+                '2xl': '1320px'
+              }
+        },
+    },
+
+    plugins: [forms],
+};
