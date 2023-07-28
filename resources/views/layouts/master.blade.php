9b0ea303fefc54d4842de0fd4cd4674758b2b77b
Ferry Febriansyah <perriebenhard@gmail.com>
2023-07-28 07:02:36 +0700

    fix: upgrade layouting

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Master Page | QiRin</title>
+    <title>{{ isset($title) ? $title : 'Testing' }} | QiRin</title>
     @include('partials.font')
     @include('partials.style')
 </head>
 <body class="font-inter">
 
-    @include('partials.header')
+    <header>
+        @yield('navbar')
+    </header>
 
     <main>
         @yield('content')
