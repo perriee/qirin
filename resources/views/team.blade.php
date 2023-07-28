6495a944f123ce910dd90c276f58738670a147b6
Ferry Febriansyah <perriebenhard@gmail.com>
2023-07-28 10:08:38 +0700

    fix: change team image url

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -13,7 +13,7 @@
       <div class="mb-6 grid gap-8 md:grid-cols-2 lg:mb-16">
         <div class="items-center rounded-lg bg-gray-50 shadow dark:border-gray-700 dark:bg-gray-800 sm:flex">
           <a href="#">
-            <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="{{ url('images/ferry.jpg') }}"
+            <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="{{ url('images/ferry-febriansyah.png') }}"
               alt="Ferry Febriansyah">
           </a>
           <div class="p-5">
@@ -27,7 +27,7 @@
         </div>
         <div class="items-center rounded-lg bg-gray-50 shadow dark:border-gray-700 dark:bg-gray-800 sm:flex">
           <a href="#">
-            <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="{{ url('images/sabiq.jpg') }}"
+            <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="{{ url('images/abdissabiq-haqi.png') }}"
               alt="Abdissabiq Haqi">
           </a>
           <div class="p-5">
@@ -41,7 +41,7 @@
         </div>
         <div class="items-center rounded-lg bg-gray-50 shadow dark:border-gray-700 dark:bg-gray-800 sm:flex">
           <a href="#">
-            <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="{{ url('images/bayu.jpg') }}"
+            <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="{{ url('images/bayu-ahmad.png') }}"
               alt="Bayu Ahmad">
           </a>
           <div class="p-5">
@@ -55,7 +55,7 @@
         </div>
         <div class="items-center rounded-lg bg-gray-50 shadow dark:border-gray-700 dark:bg-gray-800 sm:flex">
           <a href="#">
-            <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="{{ url('images/fauzan.jpg') }}"
+            <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="{{ url('images/muhammad-alfauzan.png') }}"
               alt="Muhammad Alfauzan">
           </a>
           <div class="p-5">
